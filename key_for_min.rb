b5771a769b9c59dfdd1971370fa917d873dff5f1
Valerie Moore <periwinkle17@outlook.com>
2018-06-13 20:06:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smallest = nil
   smallest_key = ""
   name_hash.each do |name, price|
-    if price < smallest
+    if smallest == nil || price < smallest
       smallest = price
       smallest_key = name
     end
