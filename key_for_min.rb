40518714450e9f4e6e407bd043db2c13d9f3e60f
Valerie Moore <periwinkle17@outlook.com>
2018-06-13 19:40:47 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  smallest = ""
+  names.each do |name, price|
+  if name == price is the lowest
+    smallest = item
+  end
+end
 
+smallest
 end
