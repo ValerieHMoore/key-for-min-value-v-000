edabe5e763debee16edc1615e569c4f2b638ebc4
Valerie Moore <periwinkle17@outlook.com>
2018-06-13 20:00:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,11 @@
 
 def key_for_min_value(name_hash)
   smallest = ""
+  smallest_key = ""
   names.each do |name, price|
+    if price < smallest
+      smallest = price
+      smallest_key = name
   if name == #price is the lowest
     smallest = item
   end
